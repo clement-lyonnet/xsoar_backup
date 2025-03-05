74691b18381ef0ee33294a52d588e233d35b2a3e
Clément Lyonnet <clement.lyonnet@gatewatcher.com>
2025-03-05 14:30:21 +0100

    fixes for retrohunt dynamic dashboard

--- Gatewatcher-AionIQ/Scripts/GatewatcherAlertEngine/GatewatcherAlertEngine.py
+++ Gatewatcher-AionIQ/Scripts/GatewatcherAlertEngine/GatewatcherAlertEngine.py
@@ -97,7 +97,7 @@ def gatewatcherAlertEngine() -> CommandResults:
             "ioc.updated_date": "",
             "ioc.usage_mode": "",
             "ioc.value": "",
-            "ioc.vulnerabilities": "",
+            "ioc.vulnerabilities": ""
         }
         ret_fields['ioc.campaigns'] = d['ioc']['campaigns']
         ret_fields['ioc.case_id'] = d['ioc']['case_id']
@@ -154,6 +154,65 @@ def gatewatcherAlertEngine() -> CommandResults:
         ret_fields['beacon.possible_cnc'] = d['beacon']['possible_cnc']
         ret_fields['beacon.session_count'] = d['beacon']['session_count']
         ret_fields['beacon.type'] = d['beacon']['type']
+
+    if d['event']['module'] == "retrohunt":
+
+        ret_fields = {
+            "ioc.campaigns": "",
+            "ioc.case_id": "",
+            "ioc.categories": "",
+            "ioc.creation_date": "",
+            "ioc.description": "",
+            "ioc.external_links": "",
+            "ioc.families": "",
+            "ioc.kill_chain_phases": "",
+            "ioc.meta_data": "",
+            "ioc.package_date": "",
+            "ioc.relations": "",
+            "ioc.signature": "",
+            "ioc.tags": "",
+            "ioc.targeted_countries": "",
+            "ioc.targeted_organizations": "",
+            "ioc.targeted_platforms": "",
+            "ioc.targeted_sectors": "",
+            "ioc.threat_actor": "",
+            "ioc.tlp": "",
+            "ioc.ttp": "",
+            "ioc.type": "",
+            "ioc.updated_date": "",
+            "ioc.usage_mode": "",
+            "ioc.value": "",
+            "ioc.vulnerabilities": "",
+            "matched_event.content": "",
+            "matched_event.id": ""
+        }
+        ret_fields['ioc.campaigns'] = d['ioc']['campaigns']
+        ret_fields['ioc.case_id'] = d['ioc']['case_id']
+        ret_fields['ioc.categories'] = d['ioc']['categories']
+        ret_fields['ioc.creation_date'] = d['ioc']['creation_date']
+        ret_fields['ioc.description'] = d['ioc']['description']
+        ret_fields['ioc.external_links'] = d['ioc']['external_links']
+        ret_fields['ioc.families'] = d['ioc']['families']
+        ret_fields['ioc.kill_chain_phases'] = d['ioc']['kill_chain_phases']
+        ret_fields['ioc.meta_data'] = d['ioc']['meta_data']
+        ret_fields['ioc.package_date'] = d['ioc']['package_date']
+        ret_fields['ioc.relations'] = d['ioc']['relations']
+        ret_fields['ioc.signature'] = d['ioc']['signature']
+        ret_fields['ioc.tags'] = d['ioc']['tags']
+        ret_fields['ioc.targeted_countries'] = d['ioc']['targeted_countries']
+        ret_fields['ioc.targeted_organizations'] = d['ioc']['targeted_organizations']
+        ret_fields['ioc.targeted_platforms'] = d['ioc']['targeted_platforms']
+        ret_fields['ioc.targeted_sectors'] = d['ioc']['targeted_sectors']
+        ret_fields['ioc.threat_actor'] = d['ioc']['threat_actor']
+        ret_fields['ioc.tlp'] = d['ioc']['tlp']
+        ret_fields['ioc.ttp'] = d['ioc']['ttp']
+        ret_fields['ioc.type'] = d['ioc']['type']
+        ret_fields['ioc.updated_date'] = d['ioc']['updated_date']
+        ret_fields['ioc.usage_mode'] = d['ioc']['usage_mode']
+        ret_fields['ioc.value'] = d['ioc']['value']
+        ret_fields['ioc.vulnerabilities'] = d['ioc']['vulnerabilities']
+        ret_fields['matched_event.content'] = d['matched_event']['content']
+        ret_fields['matched_event.id'] = d['matched_event']['id']
 
     return CommandResults(raw_response=ret_fields)
 
